3ef8268386f7a42f3e7211d420711845f2ca2f92
Madhu <madhu.kr@tayana.in>
2023-04-17 18:06:39 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>simple form </title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<center>
+<h1>Enter the details</h1>
+<form action="insert.php" method="POST">   
+<table>   
+<tr><td>Name:</td><td> <input type="text" name="name" required="required"  class="input1"/></td></tr>  
+<tr><td>Age:</td><td> <input type="number" name="age"required="required" class="input1"/></td></tr>  
+<tr><td>Company Name:</td><td> <input type="text" name="companyname" required="required" class="input1"/></td></tr>  
+</table>  
+<a href="index.html"><input type="button" value="Cancel" class="cancel"/></a>
+<input type="reset" value="Reset" class="reset" />
+<input type="submit" value="Submit" class="submit"/>  
+</form>
+</center>
+</center>
+</body>
+</html>
---
+++ insert.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Insert Page page</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body class="body1">
+	<center>
+		<?php
+
+		// servername => localhost
+		// username => root
+		// password => empty
+		// database name => staff
+		$conn = mysqli_connect("localhost", "root", "", "sample");
+		
+		// Check connection
+		if($conn === false){
+			die("ERROR: Could not connect. "
+				. mysqli_connect_error());
+		}
+		
+		// Taking all 5 values from the form data(input)
+		$name = $_REQUEST['name'];
+		$age = $_REQUEST['age'];
+		$companyname = $_REQUEST['companyname'];
+		
+		// Performing insert query execution
+		// here our table name is college
+		$sql = "INSERT INTO sample_db VALUES ('$name',
+			'$age','companyname')";
+		
+		if(mysqli_query($conn, $sql)){
+			echo "<h3>Data stored in a database successfully.</h3>";
+
+			echo nl2br("\n$name\n $age\n "
+				. "$companyname\n");
+		} else{
+			echo "ERROR: Hush! Sorry $sql. "
+				. mysqli_error($conn);
+		}
+		
+		// Close connection
+		mysqli_close($conn);
+		?>
+	</center>
+</body>
+
+</html>
