1393cbeca1923e83f9c8bc47915d9563e0f26e71
madhu-k-r <120703199+madhu-k-r@users.noreply.github.com>
2023-04-18 16:55:35 +0530

    Update insert.php

--- insert.php
+++ insert.php
@@ -16,7 +16,7 @@ $dbuser = getenv("databaseuser");
 $dbpwd = getenv("databasepassword");
 $dbname = getenv("databasename");
 
-		$conn = mysqli_connect("localhost", "root", "","sample_db");
+		$conn = mysqli_connect($dbhost, "root", "","sample_db", $dbport);
 		
 		// Check connection
 		if($conn === false){
