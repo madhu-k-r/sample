ddd3401611c3f3f0b9930420997b6aa68f59e2e3
madhu-k-r <120703199+madhu-k-r@users.noreply.github.com>
2023-04-21 11:42:11 +0530

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -3,6 +3,11 @@
 
 <head>
 	<title>Insert Page page</title>
+		<style>
+	table, th, td {
+  border: 1px solid black;
+}
+	</style>
 	<link rel="stylesheet" href="style.css">
 </head>
 
@@ -17,47 +22,41 @@ $dbpwd = getenv("databasepassword");
 $dbname = getenv("databasename");
 
 		$conn = mysqli_connect($dbhost, "$dbuser", "$dbpwd","sample_db", $dbport);
-// GET CONNECTION ERRORS
-if ($conn->connect_error) {
-	die("Connection failed: " . $conn->connect_error);
-}
-
+		if($conn === false){
+			die("ERROR: Could not connect. "
+				. mysqli_connect_error());
+		}
 // SQL QUERY
-$result = "SELECT * FROM `sample_db`;";
+$result = mysqli_query($conn,"SELECT * FROM sample_db");
 if (mysqli_num_rows($result) > 0) {
-?>
-  <table>
-  
-  <tr>
-    <td>Name</td>
-    <td>Age</td>
-    <td>Company Name</td>
-  </tr>
-<?php
-$i=0;
-while($row = mysqli_fetch_array($result)) {
-?>
-<tr>
-    <td><?php echo $row["name"]; ?></td>
-    <td><?php echo $row["age"]; ?></td>
-    <td><?php echo $row["companyname"]; ?></td>
-</tr>
-<?php
-$i++;
-}
-?>
-</table>
- <?php
-}
-else{
-    echo "No result found";
-}
-?>
-$conn->close();
-
-?>
-
-	</center>
+	?>
+		  <table>
+		  
+		  <tr>
+			<td>Name</td>
+			<td>Age</td>
+			<td>Company Name</td>
+		  </tr>
+		<?php
+			$i=0;
+			while($row = mysqli_fetch_array($result)) {
+			?>
+		<tr>
+			<td><?php echo $row["name"]; ?></td>
+			<td><?php echo $row["age"]; ?></td>
+			<td><?php echo $row["companyname"]; ?></td>
+		</tr>
+			<?php
+			$i++;
+			}
+			?>
+		</table>
+	<?php
+	}
+	else{
+		echo "No result found";
+	}
+	?>
+</center>
 </body>
-
 </html>
